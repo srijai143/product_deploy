573486b00df4814b2b4d07957ff5a8422223fdc8
srijai143 <2216116@saec.ac.in>
2025-02-03 13:54:53 +0530

    Update ProfessorRoutes.js

--- Routes/ProfessorRoutes.js
+++ Routes/ProfessorRoutes.js
@@ -3,8 +3,8 @@
 const professorController = require('../controller/ProfessorController')
 const router = require('express').Router()
 
-router.get('/api/prof_record', professorController.getAllProfessors);
-router.get('/api/prof_record/:id', professorController.getSingleProfessor)
+router.get('/api/professor', professorController.getAllProfessors);
+router.get('/api/professor/:id', professorController.getSingleProfessor)
 router.post('/api/professor', professorController.addNewProfessor)
 router.put('/api/professor', professorController.updateProfessor)
 router.delete('/api/professor/:id', professorController.deleteProfessor)
